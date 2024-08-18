be9f99cf88ebf5cddc2aa3a0a5120e4b8397efb3
tecnicoemerson <emersonrodrigues5578@gmail.com>
2024-08-17 23:03:33 -0300

    Adicionando token em uma api com security

---
+++ src/main/java/emerson/emerson/spring/security/jwt/EmersonSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package emerson.emerson.spring.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmersonSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmersonSpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/emerson/emerson/spring/security/jwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package emerson.emerson.spring.security.jwt.controller;
+
+import emerson.emerson.spring.security.jwt.model.User;
+import emerson.emerson.spring.security.jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
